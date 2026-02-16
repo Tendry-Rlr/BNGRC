bb2385f57a757fde361406760176ec363e589dc0
RAJAONSON Ny Andry Miaro <miaromiaro207@gmail.com>
2026-02-16 15:49:48 +0300

    Correction fonction getVillesByRegion

--- app/models/VilleModel.php
+++ app/models/VilleModel.php
@@ -16,7 +16,7 @@ class VilleModel
 
     public function getVillesByRegion($id_ville)
     {
-        $sql = "SELECT * FROM Ville where id_Ville= :id";
+        $sql = "SELECT * FROM Ville where id_Region= :id";
         $stmt = $this->db->prepare($sql);
         $stmt->execute([':id' => $id_ville]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
