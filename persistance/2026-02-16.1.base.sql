cbbbbaa83ddf70fa48a5d27fe80f4af3ea2edec0
RAJAONSON Ny Andry Miaro <miaromiaro207@gmail.com>
2026-02-16 13:48:30 +0300

    Ajout de la tables Don

--- persistance/2026-02-16.1.base.sql
+++ persistance/2026-02-16.1.base.sql
@@ -31,3 +31,8 @@ CREATE OR REPLACE TABLE Besoin(
     id_Besoin_Fille int references Besoin_Fille(id_Besoin_Fille),
     quantite double
 );
+
+CREATE OR REPLACE TABLE Don(
+    id_Don int primary key auto_increment,
+    id_Besoin int references Besoin(id_Besoin)
+);
