738247da61ce968c16048748a6cf094b27d4544a
MHJ <rktmihaja8@gmail.com>
2026-02-16 17:08:13 +0300

    Donnees de test pour achats

--- persistance/2026-02-16.2.base.sql
+++ persistance/2026-02-16.2.base.sql
@@ -8,5 +8,14 @@ CREATE OR REPLACE TABLE Achat(
     id_Achat int primary key auto_increment,
     id_Besoin int references Besoin(id_Besoin),
     id_Don int references Don(id_Don),
-    quantite double
+    quantite double,
+    montant double
+);
+
+CREATE OR REPLACE TABLE Achat_Entente(
+    id_Achat_Entente int primary key auto_increment,
+    id_Achat int references Achat(id_Achat),
+    partenaire varchar(100),
+    montant_entente double,
+    date_entente date
 );
