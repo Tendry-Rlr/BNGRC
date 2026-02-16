fdcc189a1807a9ce26d50e164b6e05f0fd2457f7
Tendry-Rlr <ralaiarimangatendry@gmail.com>
2026-02-16 13:47:30 +0300

    ville-region controller et views

---
+++ app/controllers/RegionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\RegionModel;
+use Flight;
+
+class RegionController
+{
+    public function listeRegion()
+    {
+        $region = new RegionModel();
+        $liste = $region->getAllRegion();
+        Flight::render('index', [
+            'listeRegion' => $liste,
+            'baseUrl' => Flight::get('flight.base_url'),
+        ]);
+    }
+}
---
+++ app/models/VilleModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\models;
+
+use Flight;
+use PDO;
+
+class VilleModel
+{
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function getVillesByRegion($id_ville)
+    {
+        $sql = "SELECT * FROM Ville where id_Ville= :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([':id' => $id_ville]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
