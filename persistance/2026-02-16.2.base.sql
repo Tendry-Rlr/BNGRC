81a66c34119c041410811c1f0d330dcc906309de
RAJAONSON Ny Andry Miaro <miaromiaro207@gmail.com>
2026-02-16 19:14:56 +0300

    Creation de la table Achat et Achat_Fille

---
+++ persistance/2026-02-16.2.base.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE TABLE Achat_Fille(
+    id_Achat_Fille int primary key auto_increment,
+    id_Besoin_Categorie int references Besoin_Categorie(id_Besoin_Categorie),
+    frais_Pourcentage double
+); 
+
+CREATE OR REPLACE TABLE Achat(
+    id_Achat int primary key auto_increment,
+    id_Besoin int references Besoin(id_Besoin),
+    id_Don int references Don(id_Don),
+    quantite double
+);
